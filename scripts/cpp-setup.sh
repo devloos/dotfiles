e344bca18b089cc17c47817293d9637f2c3238d3
Carlos Aguilera <18aguilerac@gmail.com>
2022-08-28 22:37:46 -0700

    updating scripts

--- scripts/cpp-setup.sh
+++ scripts/cpp-setup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-mkdir include build bin lib src .vscode
-touch src/main.cpp CmakeLists.txt .vscode/c_cpp_properties.json
-touch .vscode/launch.json .vscode/settings.json bin/.gitkeep
-touch include/.gitkeep lib/.gitkeep
-touch .clang-format temp.txt
+Make_Dirs() {
+  mkdir $1 $2 $3 $4 $5 $6 $7
+}
 
-gitignore.sh
-helperCmake.sh
-helperVscode.sh
-helperClangFormat.sh
+Make_Dirs include build bin lib src src/core .vscode
+
+Touch_File() {
+  touch $1 $2 $3 $4 $5
+}
+
+Touch_File src/core/Application.cpp CMakeLists.txt .vscode/c_cpp_properties.json .vscode/launch.json .vscode/settings.json
+Touch_File bin/.gitkeep include/.gitkeep lib/.gitkeep .clang-format temp.txt
+
+hGitignore.sh
+hCmake.sh
+hVscode.sh
+hClangFormat.sh
 
 sed "s/\"/'/g" temp.txt > .clang-format
 rm temp.txt
--- scripts/hCmake.sh
+++ scripts/hCmake.sh
@@ -5,19 +5,24 @@ set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_BUILD_TYPE Debug)
 
-project(main)
+project(App)
 
-add_executable(${PROJECT_NAME} src/main.cpp)
-target_include_directories(${PROJECT_NAME} PUBLIC /include)
+add_executable(${PROJECT_NAME} src/core/Application.cpp)
 
 # Letting CMake find library
 # find_library(${LIB_NAME} ${LIB_TO_FIND} PATHS /tmp/customPath)
 # User explicitly finding library
 # target_include_directories(${PROJECT_NAME} PUBLIC ${LIB_HEADER_FILES})
-# target_link_libraries(${PROJECT_NAME} PUBLIC ${LIB_TO_INCLUDE})' > CmakeLists.txt
+# target_link_libraries(${PROJECT_NAME} PUBLIC ${LIB_TO_INCLUDE})' > CMakeLists.txt
 
-echo '#include <iostream>
+echo '#ifndef APPLICATION_H_
+#define APPLICATION_H_
+#include <iostream>
+
+#endif // APPLICATION_H_' > src/core/Application.h
+
+echo '#include "Application.h"
 
 int main() {
   return 0;
-}' > src/main.cpp
+}' > src/core/Application.cpp
