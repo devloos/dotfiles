341fc7d63d4bf6cbc7816297e4166851d8c736a8
Carlos Aguilera <18aguilerac@gmail.com>
2022-10-31 14:53:02 -0700

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+# Config
+alias nvimrc="nvim ~/.config/nvim/init.lua"
+alias fishrc="nvim ~/.config/fish/config.fish"
+alias gitrc="nvim ~/.gitconfig"
+
+# Movement
+alias l="ls -la"
+alias ...="cd ../.."
+alias ..="cd .."
+
+# Git
+alias ga="git add"
+alias gp="git push"
+alias gcm="git commit -m"
+alias gti="git"
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    eval $(/opt/homebrew/bin/brew shellenv)
+end
