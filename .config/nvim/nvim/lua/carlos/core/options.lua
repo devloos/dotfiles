5304d751b14ec6f3abb556ee8516cac58102698a
Carlos Aguilera <18aguilerac@gmail.com>
2023-03-05 15:32:50 -0800

    update

---
+++ .config/nvim/nvim/lua/carlos/core/options.lua
@@ -0,0 +1,31 @@
+-- Vim opt
+local opt = vim.opt
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.backup = false
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+
+-- line numbers
+opt.number = true
+opt.numberwidth = 4
+opt.relativenumber = true
+
+-- cursor line
+opt.cursorline = true
+opt.cc = "80"
+opt.mouse = "a"
+
+opt.iskeyword:append("-")
+
+
+-- vim.opt.clipboard = "unnamedplus" -- this is one way clipboard copying
+opt.clipboard = "unnamed" -- this is two way clipboard copying. beware everything outside of vim register will be there now
+
+-- Theme
+-- vim.o.background = 'dark'
+-- require('vscode').setup{} -- vscode theme 
+-- vim.cmd([[colorscheme gruvbox]])
