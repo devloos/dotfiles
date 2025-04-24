6920d2b1645e662f697d5025b5cc05af44f89f87
Carlos Aguilera <18aguilerac@gmail.com>
2025-04-24 12:05:05 -0700

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,11 @@
+dofile(vim.fn.stdpath("config") .. "/settings.lua")
+
 -- Bootstrap lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
@@ -25,14 +28,6 @@ require("lazy").setup({
 		end,
 	},
 
-	{
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter",
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
-
 	{ "windwp/nvim-autopairs", opts = {} },
 
 	-- Tree-sitter for syntax highlighting and more
@@ -43,7 +38,6 @@ require("lazy").setup({
 			require("nvim-treesitter.configs").setup({
 				highlight = { enable = true },
 				indent = { enable = true },
-				autotag = { enable = true },
 			})
 		end,
 	},
@@ -82,7 +76,47 @@ require("lazy").setup({
 					typescript = { "prettier" },
 					html = { "prettier" },
 					css = { "prettier" },
+					vue = { "prettier" },
 				},
+			})
+		end,
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- LSP source for cmp
+			"hrsh7th/cmp-buffer", -- buffer words
+			"hrsh7th/cmp-path", -- file paths
+			"hrsh7th/cmp-cmdline", -- command line completions
+			"L3MON4D3/LuaSnip", -- snippet engine
+			"saadparwaiz1/cmp_luasnip", -- snippet completions
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-;>"] = cmp.mapping.complete(),
+					["<Tab>"] = cmp.mapping.confirm({ select = true }),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+					{ name = "path" },
+				}),
 			})
 		end,
 	},
@@ -92,8 +126,10 @@ require("lazy").setup({
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
 				settings = {
 					Lua = {
 						diagnostics = {
@@ -117,47 +153,3 @@ require("lazy").setup({
 		end,
 	},
 })
-
--- General settings
-vim.opt.termguicolors = true
-
--- tabs & indentation
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.backup = false
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-
--- line numbers
-vim.opt.number = true
-vim.opt.numberwidth = 4
-vim.opt.relativenumber = true
-
--- cursor line
-vim.opt.mouse = "a"
-
-vim.opt.clipboard = "unnamed" -- this is two way clipboard copying.
-
--- Leader key
-vim.g.mapleader = " "
-
--- Keymaps for Telescope
-vim.keymap.set("n", "<leader>ff", function()
-	require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })
-end, { noremap = true })
-
-vim.keymap.set("n", "<leader>fg", function()
-	require("telescope.builtin").live_grep({ cwd = vim.fn.getcwd() })
-end, { noremap = true })
-
--- Motion
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("n", "<C-d>", "<C-d>zz") -- cursor stays in middle
-vim.keymap.set("n", "<C-u>", "<C-u>zz") -- when halfpage jumping
-vim.keymap.set("n", "<S-g>", "<S-g>zz") -- cursor stays in middle
-vim.keymap.set("n", "}", "}zz") -- cursor stays in middle
-vim.keymap.set("n", "{", "{zz") -- cursor stays in middle
-
-vim.keymap.set("x", "<leader>p", '"_dP')
